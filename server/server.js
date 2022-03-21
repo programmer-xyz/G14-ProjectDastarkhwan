e5283b3e708f5353a9681445458bc6afe20ec0fc
MukRaza <Mukeet@tcig.co>
2022-03-21 15:11:23 +0500

    path error fixed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db";
 
 //configure env file
-dotenv.config({path : "/Users/emaanmujahid/Desktop/G14-ProjectDastarkhwan/server/config/config.env"});
+dotenv.config({path : "../server/config/config.env"});
 connectDB();
 
 app.listen(process.env.PORT, () =>{
