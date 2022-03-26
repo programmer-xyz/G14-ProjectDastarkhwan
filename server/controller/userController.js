efa2652af307c9c1def259f29b91693f31f54e30
Emaan <23100276@lums.edu.pk>
2022-03-27 00:35:37 +0500

    user login done

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,22 +1,22 @@
-import Ngo from "../models/ngoSchema.mjs";
+import User from "../models/userSchema.mjs";
 import { handleAsyncErr } from "../middleware/handleAsyncErr.js";
 import bcrypt from "bcrypt";
 import HandErr from "../utils/err.js";
 
 //login 
-export const ngoLogin =  handleAsyncErr(async (req,res, next) =>{
+export const userLogin =  handleAsyncErr(async (req,res, next) =>{
     const {email, password} = req.body;
 
     if(!email || !password){
        
         return next(new HandErr("email or password missing", 400))
     }
-    const user = await Ngo.findOne({email}); //.select("+password");
+    const user = await User.findOne({email}); 
     let pw = user.password;
 
     if(!user){
        
-        return next(new HandErr("Wrong userName or password", 401));
+        return next(new HandErr("Wrong email or password", 401));
     }
     let boolCheck  = await bcrypt.compare(password, pw);
     //boolCheck = user.password == password ? true : false; //add bcrypt here
@@ -33,9 +33,3 @@ export const ngoLogin =  handleAsyncErr(async (req,res, next) =>{
     }
     
 });
-
-export const ngoRegister = handleAsyncErr(async (req, res, next) =>{
-    const {name, email, phoneNumber, password, address,description, contactNum, contactName, contactEmail, accountNum, VerifiDoc} = req.body;
-
-});
-
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import {createAdmin, adminLogin, approvePost} from "../controller/authenicateController"
+import {createAdmin, adminLogin, approvePost, rejectPost} from "../controller/authenicateController"
 
 const router = Router();
 
 router.route("/admin").post(createAdmin);
 router.route("/adminLogin").post(adminLogin);
 router.route("/approve").get(approvePost);
-
+router.route("/reject").get(rejectPost);
 
 export default router;
