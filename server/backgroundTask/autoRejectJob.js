6919a7ba48265aa2d9591d96eaa1c632ef652ad7
MukRaza <Mukeet@tcig.co>
2022-04-06 01:40:28 +0500

    background Task for auto reject application

--- server/backgroundTask/autoRejectJob.js
+++ server/backgroundTask/autoRejectJob.js
@@ -1,8 +1,20 @@
 import cron from 'node-cron'
-
+import application from "../models/applicationSchema.mjs";
 export const rejectApplication = ()=>
 {
-    cron.schedule('* * * * *', function(){
-        console.log("task running ever min");
+    cron.schedule('* * * * *', async function(){
+        console.log("here")
+        const response = await application.updateMany(
+            {"createdAt": {
+            $gte: new Date((new Date().getTime() - (3 * 24 * 60 * 60 * 1000))),
+            $lt: new Date((new Date().getTime() - (2 * 24 * 60 * 60 * 1000)))
+          },
+        "approvalStatus": "inProgress"},
+        {
+            $set: { approvalStatus:"rejected"}
+        }
+    )
+     
+        console.log(response);
     })
 }
