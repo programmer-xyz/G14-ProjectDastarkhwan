15ecbf4baa6e7e3f61ba3fc30f028b8f12f838c2
khuzaima-saeed <khuzaimasaeed@gmail.com>
2022-04-06 19:12:51 +0500

    khuzi's 1st

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "body">
+      <div className = "left-container">
+        <div className = "btn-box">
+          <button id = "btn1">User</button>
+          <button id = "btn2">Restaurant</button>
+          <button id = "btn3">NGO</button>
+        </div>
+        <div id = "content1" className ="content">
+          <form>
+          <input type = "text" placeholder = "Email"/>
+          <p>&nbsp;</p>
+          <input type = "text" placeholder = "Password"/>
+          <p>&nbsp;</p>
+          <button className = "sign-in" type = "submit"> SIGN IN</button>
+          </form>
+        </div>
+      </div>
+      <div className = "right-container"></div>
+      <script src = "loginScript.js"></script>
     </div>
   );
 }
-
 export default App;
---
+++ client/src/loginScript.js
@@ -0,0 +1,7 @@
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+
+btn1.addEventListener('click', () => btn1.style.color = '#ff7846')
+btn2.addEventListener('click', () => btn2.style.color = '#ff7846')
+btn3.addEventListener('click', () => btn3.style.color = '#ff7846')
