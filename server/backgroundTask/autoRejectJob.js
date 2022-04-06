13c6ec2a6650da368aa3fc813bee984788244668
MukRaza <Mukeet@tcig.co>
2022-04-06 10:04:39 +0500

    added comments to autoRejectTask

--- server/backgroundTask/autoRejectJob.js
+++ server/backgroundTask/autoRejectJob.js
@@ -1,7 +1,13 @@
 import cron from 'node-cron'
 import application from "../models/applicationSchema.mjs";
+
+/* this is a node-corn job  that is currently ping every sec for testing purpose has 
+to be changed to every day/3 hours depending on what time decide optimal time to check
+for this background task  
+*/
 export const rejectApplication = ()=>
 {
+    /* automated rjection of application if number of days for application is greater or equal to 3 days */
     cron.schedule('* * * * *', async function(){
         console.log("here")
         const response = await application.updateMany(
