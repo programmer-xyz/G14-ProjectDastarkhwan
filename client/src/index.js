24aa8af0970249bfa8125e1072ee44211260943b
MukRaza <Mukeet@tcig.co>
2022-04-09 11:49:06 +0500

    file test

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './pages/loginpage/login.js';
 import Navbar from "./components/Navbar/Navbar";
-import ForgetPassword from "./pages/forgetpassword/forgetpassword.js";
+// import ForgetPassword from "./pages/forgetpassword/forgetpassword.js";
 
 ReactDOM.render(
-    // <Navbar/>,
-    // <Login />,
-    <ForgetPassword />,
+    <Navbar/>,
+    <Login />,
+    //<ForgetPassword />,
   document.getElementById('root')
 );
