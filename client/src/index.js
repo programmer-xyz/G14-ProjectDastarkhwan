d50705be275d9f9837b6187d116c4ad90fa087ae
AbdulMuizzk <23100153@lums.edu.pk>
2022-04-09 21:28:03 +0500

    done

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,5 @@ import TabBar from './components/TabBar/TabBar.jsx';
 
 ReactDOM.render(
   <App/>,
-  // <Cards Resturant={1} mealsDonated={10} ngoDonatedTo={10} monetaryDonations={20} mealDonations={40} rationDonations={10}/>,
-  // <TabBar itemOne={"Monetary"} itemTwo={"Ration"} itemThree={"Meals"} Restuarant={false}/>,
-  
   document.getElementById('root')
 );
