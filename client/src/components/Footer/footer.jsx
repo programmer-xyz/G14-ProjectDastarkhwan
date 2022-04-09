2ca567e0fc623e704f6eb0b5e6772cc1443ccda4
AbdulMuizzk <23100153@lums.edu.pk>
2022-04-09 23:56:35 +0500

    fixed responsiveness

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -8,24 +8,24 @@ const FooterPage = (props) => {
     return (
     <div class="pt-5 pb-2 footer" style={{background:props.user?'#FCEEE1':'#B2DCD6'}}>
             <div class="row hel">
-                <div class="col-lg-5 col-xs-12 about-company">
-                <h2>Alone we can do little,<br/>together we can do so much!</h2>
-                <img src ={logo} alt="logoofdastarkhwaan"/>
-                </div>
-                <div class="col-lg-4 col-xs-12 location">
-                    <h4 class="mt-lg-0 mt-sm-4 heads">Site Links</h4>
-                    <a>Users</a>
-                    <br/>
-                    <a>Resturants</a>
-                    <br/>
-                    <a>NGOs</a>
-                </div>
+                <div class="col-lg-6 col-sm-12 col-md-6 about-company">
+                    <h2>Alone we can do little,<br/>together we can do so much!</h2>
+                    <img src ={logo} alt="logoofdastarkhwaan"/>
+                    </div>
+                    <div class="col-lg-4 col-xs-12 location">
+                        <h4 class="mt-lg-0 mt-sm-4 heads">Site Links</h4>
+                        <a>Users</a>
+                        <br/>
+                        <a>Resturants</a>
+                        <br/>
+                        <a>NGOs</a>
+                    </div>
             </div>
             <div class="row hel">
                 <div class="col copyright">
                     <p class="">Copyright © 2022 Dastarkhwaan. All Rights Reserved.</p>
                 </div>
-                <div class="col-lg-4 col-xs-12 followus">
+                <div class="col-lg-6 col-sm-12 col-md-6 followus">
                     <h4 class="heads">Follow us</h4>
                     <FaFacebookF size={24} className='mr-3'/>
                     <FaTwitter size={24} className='mr-3'/>
