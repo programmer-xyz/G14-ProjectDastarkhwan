eed4ea10521cea384a2e5f9101b53db21637d7ce
khuzaima-saeed <khuzaimasaeed@gmail.com>
2022-04-11 20:13:49 +0500

    Forms2

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Login from './pages/loginpage/login.js';
 import Cards from './components/Cards/cards.jsx';
 import Forms from './components/Forms/forms.jsx';
+import FormsDonation from './components/Forms/formsDonation.jsx';
 //import pages here and call then the way login has been called
 const App = () => {
     return(
@@ -9,7 +10,7 @@ const App = () => {
             <div className="App">
                 <Routes> 
                     {/* <Route path='/' element={<Login/>}/> */}
-                    <Route path='/' element={<Forms User = {1}/>}/>
+                    <Route path='/' element={<FormsDonation User = {1}/>}/>
                     {/* <Route path='/' element={<Cards Resturant={0} mealsDonated={10} ngosDonatedTo={10} monetaryDonations={20} mealDonations={40} rationDonations={10}/>}/> */}
                 </Routes>
             </div>
---
+++ client/src/components/Forms/formsDonation.jsx
@@ -0,0 +1,98 @@
+import './formsDonation.css';
+import logo from '../../assets/MaskGroup3.png';
+
+function FormsDonation (props) {
+    if (props.User === 1){
+        return(
+                <div class = "row newClass">
+                    <form>
+                        <div class="form-group">
+                            <select class="form-select shadow-none" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="NGO">
+                                <option selected>Select NGO</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleInputPassword1" placeholder="Amount ($)"/>
+                        </div>
+                        <div class = "details">Card Details</div>
+                        <div class="form-group">
+                            {/* <label>Card Details</label> */}
+                            <input type="text" class="form-control shadow-none" id="exampleCheck1" placeholder="Name On Card"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleCheck1" placeholder="Card Number"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleCheck1" placeholder="Expiry Date"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleCheck1" placeholder="CVC"/>
+                        </div>
+                        <button type = "submit" class="buttons">Confirm Donation!</button>
+                    </form>
+                </div>
+        );
+    }
+    else if (props.User === 2){
+        return (
+            <div class = "row newClass">
+                    <div class= "col-lg-6 col-xs-12 col-md-12 col-sm-12">
+                    <form>
+                        <div class="form-group">
+                            <select class="form-select shadow-none" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="NGO">
+                                <option selected>Select NGO</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleInputPassword1" placeholder="Address (House No, St. Block, City, Country)"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleCheck1" placeholder="Description"/>
+                        </div>
+                        <div class="form-group">
+                            <label>Upload Ration Image</label>
+                            <input type="file" class="form-control shadow-none" id="exampleCheck1" placeholder="Add Ration Image"/>
+                        </div>
+                        <button type = "submit" class="buttons">Confirm Donation!</button>
+                    </form>
+                    </div>
+                </div>
+        );
+    }
+    else if (props.User === 3){
+        return (
+            <div class = "row newClass">
+                    <div class= "col-lg-6 col-xs-12 col-md-12 col-sm-12">
+                    <form>
+                        <div class="form-group">
+                            <select class="form-select shadow-none" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="NGO">
+                                <option selected>Select NGO</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleInputPassword1" placeholder="Address (House No, St. Block, City, Country)"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-none" id="exampleCheck1" placeholder="Description"/>
+                        </div>
+                        <div class="form-group">
+                            <label>Upload Meal Image</label>
+                            <input type="file" class="form-control shadow-none" id="exampleCheck1" placeholder="Add Ration Image"/>
+                        </div>
+                        <button type = "submit" class="buttons">Confirm Donation!</button>
+                    </form>
+                    </div>
+                </div>
+        );
+    }
+}
+export default FormsDonation;
