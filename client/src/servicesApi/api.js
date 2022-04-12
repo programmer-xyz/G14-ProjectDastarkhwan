003e7f3d43726884bb9c1420d02b66364591075f
MukRaza <Mukeet@tcig.co>
2022-04-12 22:49:13 +0500

    taking pull

--- client/src/servicesApi/api.js
+++ client/src/servicesApi/api.js
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 export const createAccountUser =  async (name,username,email,phoneNumber,paswword,cnic,address,Description) =>
 {
-    
+    // const user1 ={
+    //     'name':name,
+    //     "username":"",
+    //     "email":"",
+    //     "phoneNum":"",
+    //     "paswword":"",
+    //     cnic:"",
+    //     address:"",
+    //     // HouseNo:"",
+    //     // stBlock:"",
+    //     // city:"",
+    //     // country:"",
+    //     description:""
+    // }
 }
 
