6a73deedb780722b53a2331fbe3702114640c169
Emaan <23100276@lums.edu.pk>
2022-04-12 15:54:23 +0500

    api updated

--- client/src/servicesApi/api.js
+++ client/src/servicesApi/api.js
@@ -1,15 +1,3 @@
 import axios from 'axios'
 
-export const LogIn = async (Email, Password) => {
 
-    const object = {
-        "email" : Email,
-        "password":Password
-    }
-
-    return await axios.post(`${Url}/userController/login`,object,{
-        'Accept': 'application/json',
-        'content-type':'application/json'
-
-    });
-}
