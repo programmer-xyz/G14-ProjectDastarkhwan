b09e991502b5e4b6d48e13776f5c3b06e2441cae
MukRaza <Mukeet@tcig.co>
2022-04-12 23:58:37 +0500

    add registration intregation for ngo

--- client/src/servicesApi/api.js
+++ client/src/servicesApi/api.js
@@ -19,8 +19,56 @@ export const createAccountUser =  async (name,username,email,phoneNumber,passwor
     return await axios.post(`${Url}/user/register`,obj);
 }
 
-export const createNgoUser =  async (name,username,email,phoneNumber,paswword,cnic,address,Description) =>
+export const createNgoUser =  async (name,username,email,phoneNumber,paswword,city,country,streetNumber,houseNumber,contactNum,contactName,contactEmail,accountNum,Description,file) =>
 {
+    const obj = {
+    "name":name, 
+    "email":email, 
+    "phoneNumber":phoneNumber, 
+    "password":paswword,
+    "address":{
+        "city":city,
+        "country":country,
+        "streetNumber":streetNumber,
+        "houseNumber":houseNumber
+    },
+    "description":Description, 
+    "contactNum":contactNum, 
+    "contactName":contactName, 
+    "contactEmail":contactEmail,
+    "accountNum":accountNum, 
+    "userName":username,
+    "verifyDoc":file
+    } 
 
-  
+    return await axios.post(`${Url}/ngo/register`,obj,{ headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+}
+
+export const createRestUser = async (name,username,email,phoneNumber,paswword,city,country,streetNumber,houseNumber,contactNum,contactName,contactEmail,accountNum,Description)=>
+{
+    const obj = {
+        "name":name, 
+        "email":email, 
+        "phoneNumber":phoneNumber, 
+        "password":paswword,
+        "address":{
+            "city":city,
+            "country":country,
+            "streetNumber":streetNumber,
+            "houseNumber":houseNumber
+        },
+        "description":Description, 
+        "contactNum":contactNum, 
+        "contactName":contactName, 
+        "contactEmail":contactEmail,
+        "accountNum":accountNum, 
+        "userName":username
+        }
+    return await axios.post(`${Url}/rest/register`,obj,{ headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+    });
 }
