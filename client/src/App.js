faa2b562bff635cf82db5a79e5508251803466bb
Emaan <23100276@lums.edu.pk>
2022-04-13 22:55:14 +0500

    route fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Login from './pages/loginpage/login.js';
 import Cards from './components/Cards/cards.jsx';
 import FooterPage from './components/Footer/footer.jsx';
-import TabBar from './components/TabBar/TabBar.jsx';
+
 import RequestItems from './components/RequestItems/requestItems.jsx';
 import NavBar from './components/Navbar/Navbar.jsx'
 import image1 from './components/RequestItems/testImage.jpeg';
@@ -25,7 +25,7 @@ const App = () => {
                 <Routes> 
                     <Route path='/' element={<Login/>}/>
                     <Route path="/Signup" element= {<Signup/>}/>  
-
+                    <Route path = "/donationForms" element= {<DonationForms Resturant={true} Number = {2}/>} />
                 
                     <Route path='/Cards' element={<Cards Resturant={2} mealsDonated={10} ngosDonatedTo={10} monetaryDonations={20} mealDonations={40} rationDonations={10}/>}/>
                     <Route path="/footer" element={<FooterPage user={false}/>} />
@@ -33,7 +33,7 @@ const App = () => {
                     <Route path="/modelitems" element = {<RequestItems image={image1} name={"Abdul Muizz khan"} heading1={"Type"} heading2={"Date"} pargaraph1={"Meal Donations"} date={"7/03/4"} time={"10:47 am"} userRequests={true} NGOrequests={false} Resreq={true} buttonStat={0}/>} />
                     <Route path = "/modals" element = {<Modals/>} />
                     <Route path ="/forms" element = {<FormsDonation User={1} />} />
-                    <Route path = "donationForms" element= {<DonationForms Resturant={true} Number = {2}/>} />
+   
                 </Routes>
             </div>
         </Router>
