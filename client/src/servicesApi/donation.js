0013fea0028534dd46af2b249c761d5e88360588
Emaan <23100276@lums.edu.pk>
2022-04-13 23:57:17 +0500

    donations middleware complete

--- client/src/servicesApi/donation.js
+++ client/src/servicesApi/donation.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Url = "http://localhost:8080/api/v1";
 
-export const MealDonation =  async (address, description, email, ngoIdentifier, image) =>{
+export const mealDonationUser =  async (address, description, email, ngoIdentifier, image) =>{
     const obj = {
 
     "email":email, 
@@ -13,5 +13,47 @@ export const MealDonation =  async (address, description, email, ngoIdentifier, 
 
 }
 
+    return await axios.post(`${Url}/user/mealDonation`,obj);
+}
+
+export const rationDonationUser =  async (address, description, email, ngoIdentifier, image) =>{
+    const obj = {
+
+    "email":email, 
+    "description":description,
+    "ngoIdentifier": ngoIdentifier,
+    "image":image,
+    "address":address 
+
+}
+
+    return await axios.post(`${Url}/user/rationDonation`,obj);
+}
+
+export const moneyDonationUser =  async (email, ngoIdentifier, amount, cardNum) =>{
+    const obj = {
+
+    "email":email, 
+    "ngoIdentifier": ngoIdentifier,
+    "amount": amount,
+    "cardNum": cardNum
+}
+
     return await axios.post(`${Url}/user/moneyDonation`,obj);
+}
+
+//restaurant donation 
+
+export const mealDonationRest =  async (address, description, email, ngoIdentifier, image) =>{
+    const obj = {
+
+    "email":email, 
+    "description":description,
+    "ngoIdentifier": ngoIdentifier,
+    "image":image,
+    "address":address 
+
+}
+
+    return await axios.post(`${Url}/rest/mealDonation`,obj);
 }
