b59cd8ac0b5ba2238fd18786599c9532118c8fa1
khuzaima-saeed <khuzaimasaeed@gmail.com>
2022-04-17 20:47:54 +0500

    navbar changed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React,{useState} from 'react';
 import { AppBar,Toolbar,Box} from '@mui/material';
-import './Navbar.css'
+import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
+import SignOutModals from '../Modals/SignOutModal';
 
-function Navbar() {
+function Navbar(props) {
+    const navigation = useNavigate();
     const [button,setbuttonState] = useState({
         button1:true,
         button2:false
     });
-    function handleClick(e){
+    const [modalState,setModalState] = useState(false)
+    const HandleClick = (props,e)=>{
         var obj = {}
         if(e.target.value === "Home")
         {
@@ -15,7 +19,6 @@ function Navbar() {
                 button1:true,
                 button2:false
             }
-
         }
         else if (e.target.value ==="Profile")
         {
@@ -25,16 +28,70 @@ function Navbar() {
             }
         }
         console.log("this is button",obj)
-        setbuttonState(obj)
+        setbuttonState(obj);
+        RedirectClick(props,e);
+    }
+
+    function onClick2 (){
+
+        navigation("/");
+
+    }
+
+    function openModal (){
+        setModalState(true);
+    }
+
+    function handleClose(reason)
+    {
+    if (reason !== "backdropClick")
+    {
+        return 
+    }
+    setModalState(false);
+    }
+
+    function onClick()
+    {
+    setModalState(false);
+    }
+
+    const RedirectClick = (props,e) => {
+        if (e.target.value === "Home"){
+            if (props.User === 0){
+                navigation ('/userdashboard')
+            }
+            else if (props.User === 1){
+                navigation ('/resturantDashboard')
+            }
+            else if (props.User === 2){
+                navigation ('/ngodashboard')
+            }
+            else if (props.User === 3){
+                navigation ('/adminDashboard')
+            }
+        }
+        else if (e.target.value === "Profile"){
+            console.log("here")
+            if (props.User === 0){
+                navigation ('/userprofile')
+            }
+            else if (props.User === 1){
+                navigation ('/resprofile')
+            }
+            else if (props.User === 2){
+                navigation ('/ngoprofile')
+            }
+        }
     }
 return (
         <AppBar position={"static"} sx={{flexGrow:1, background:'transparent',width:"100%",alignContent:"right",alignItems:"right"}} elevation={0}>
+            <SignOutModals state={modalState} modelMsg = {"Are you sure you want to sign out?" } actionMsg={"Cancel"} actionMsg2={"Sign Out"} handleClose = {handleClose} onClick = {onClick} onClick2={onClick2}/>
             <Toolbar sx={{justifyContent:"right",alignItems:"right"}}>
-                
                     <Box sx={{justifyContent:"right",alignItems:"right"}}>
-                    <button onClick={(e)=>{handleClick(e)}} className = {"buttons1"+(button.button1?" orangeColor":" greenColor")} value="Home">Home</button>
-                    <button onClick={(e)=>{handleClick(e)}} className = {"buttons1"+(button.button2?" orangeColor":" greenColor")} value="Profile">Profile</button>
-                    <button className="buttons2" type="submit">Sign Out</button>
+                    <button onClick = {(e) => HandleClick(props,e)} className = {"buttons1"+(button.button1?" orangeColor":" greenColor")} value="Home">Home</button>
+                    <button onClick = {(e) => HandleClick(props,e)} className = {"buttons1"+(button.button2?" orangeColor":" greenColor")} value="Profile">Profile</button>
+                    <button onClick = {openModal} className="buttons2" type="submit">Sign Out</button>
                     </Box>
                 
                 
