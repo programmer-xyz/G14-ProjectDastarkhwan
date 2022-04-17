60f5f51d8df9f5b6235ec08ffdf713348fcdb720
Emaan <23100276@lums.edu.pk>
2022-04-17 15:14:17 +0500

    admin routed added

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import {createAdmin, adminLogin, approvePost, rejectPost, viewApplications,startApp,logOut} from "../controller/authenicateController"
-
+import {isAuthenticatedUser} from "../middleware/auth";
 const router = Router();
 
-router.route("/create").post(createAdmin);
-router.route("/login").post(adminLogin);
-router.route("/approve").get(approvePost);
-router.route("/reject").get(rejectPost);
-router.route("/viewApplications").get( viewApplications);
-router.route("/generalLogOut").get( logOut);
+router.route("/create").post(isAuthenticatedUser,createAdmin);
+router.route("/login").post(isAuthenticatedUser,adminLogin);
+router.route("/approve").get(isAuthenticatedUser,approvePost);
+router.route("/reject").get(isAuthenticatedUser,rejectPost);
+router.route("/viewApplications").get(isAuthenticatedUser, viewApplications);
+router.route("/generalLogOut").get( isAuthenticatedUser,logOut);
 router.route("/").get(startApp);
 export default router;
 
